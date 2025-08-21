203248bac71838a2dfe1d1c04053e731f0ccdee5
joohee1-kim <joohee1.kim@partner.samsung.com>
2025-08-21 19:18:15 +0900

    practice

---
+++ studying/ch02/p-53.or_gate.py
@@ -0,0 +1,15 @@
+# p.51
+## 2.3. 퍼셉트론 구현하기
+## 2.3.3. 가중치와 편향 구현하기
+
+import numpy as np
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
