c3cd4b0fcbd0bdc442899c7e1ef3b0b854fa416c
joohee1-kim <joohee1.kim@partner.samsung.com>
2025-08-26 20:39:05 +0900

    perceptron

--- studying/ch02/p-53.and_gate.py
+++ studying/ch02/p-53.and_gate.py
@@ -5,9 +5,9 @@
 import numpy as np
 
 def AND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.7
+    x = np.array([x1, x2])      # 입력
+    w = np.array([0.5, 0.5])    # 가중치
+    b = -0.7                    # 편향
     tmp = np.sum(w*x) + b
     if tmp <= 0:
         return 0
